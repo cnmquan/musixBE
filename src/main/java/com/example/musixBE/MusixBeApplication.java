425d0ada948b33a21ae8b7b7acfeab868b7b9fdc
Minh Quan <minhquancn1809@gmail.com>
2023-02-17 09:12:33 +0700

    170223 initialize backend folder

---
+++ src/main/java/com/example/musixBE/MusixBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.musixBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusixBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusixBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/musixBE/MusixBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musixBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusixBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
