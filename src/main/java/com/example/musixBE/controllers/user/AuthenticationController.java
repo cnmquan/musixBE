29c3f011424b1eae21e1d8f70a49b955cba66a01
nguyen don thinh <thinhnguyendoan5122001@gmail.com>
2023-06-27 13:59:58 +0700

    update

--- src/main/java/com/example/musixBE/controllers/user/AuthenticationController.java
+++ src/main/java/com/example/musixBE/controllers/user/AuthenticationController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
