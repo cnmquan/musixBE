595827f1be6f6b806ebdfe631b9d6c6f22a506ea
nguyen don thinh <thinhnguyendoan5122001@gmail.com>
2023-06-28 16:03:47 +0700

    update\

--- src/main/java/com/example/musixBE/MusixBeApplication.java
+++ src/main/java/com/example/musixBE/MusixBeApplication.java
@@ -1,9 +1,16 @@
 package com.example.musixBE;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@EnableAutoConfiguration
+@EnableWebMvc
+@Configuration
+@ComponentScan
 public class MusixBeApplication {
 
     public static void main(String[] args) {
